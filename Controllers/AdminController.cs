0d6f72cd9116a41b48b3cdb8d19ba9fbd43aee73
Augusto-Camani <augustocamani@gmail.com>
2024-04-11 16:56:54 -0300

    Swagger functioning/Controller added

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rodriguez_Camani_Feresin_Backend;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AdminController:ControllerBase
+{
+    private readonly IAdminService _adminService;
+
+    public AdminController(IAdminService adminService)
+    {
+        _adminService = adminService;
+        
+    }
+
+    [HttpGet("getAdmins")]
+    public IActionResult GetAdmins(){
+        return Ok(_adminService.GetAll());
+    }
+
+}
