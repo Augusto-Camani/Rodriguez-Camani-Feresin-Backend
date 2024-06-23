b38e93c2dd3f353f95646c8ec1cc18144376ddad
Augusto-Camani <augustocamani@gmail.com>
2024-06-23 15:45:53 -0300

    ScheduleService

--- obj/Debug/net8.0/Rodriguez-Camani-Feresin-Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Rodriguez-Camani-Feresin-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rodriguez-Camani-Feresin-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff17d28b1dbfa9a0e212d21aa86cc91b9bc5d9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b5ae652e3352ec8d403621f7f2fdc1d1cc03a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rodriguez-Camani-Feresin-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rodriguez-Camani-Feresin-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
