010a59651f4aac717b3f671eb7456ea8604b6460
Augusto-Camani <augustocamani@gmail.com>
2024-06-28 11:50:49 -0300

    Assembly references

--- obj/Debug/net8.0/Rodriguez-Camani-Feresin-Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Rodriguez-Camani-Feresin-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rodriguez-Camani-Feresin-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30970d0644bf63f89a4cbbaddbf0db51965798f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc6d4d443beb944bf95e1784dd62fe2493543c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rodriguez-Camani-Feresin-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rodriguez-Camani-Feresin-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
