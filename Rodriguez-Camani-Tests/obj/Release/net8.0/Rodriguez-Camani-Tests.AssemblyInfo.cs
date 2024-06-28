5f57dc340584d38efae366abf33f28d8c9372f71
Fernando Rodriguez <fr30031@gmail.com>
2024-06-28 11:49:20 -0300

    Resolve error test dependencies

--- Rodriguez-Camani-Tests/obj/Release/net8.0/Rodriguez-Camani-Tests.AssemblyInfo.cs
+++ Rodriguez-Camani-Tests/obj/Release/net8.0/Rodriguez-Camani-Tests.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rodriguez-Camani-Tests")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc6d4d443beb944bf95e1784dd62fe2493543c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rodriguez-Camani-Tests")]
